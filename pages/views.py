4ff2eaba967e312a9f8adba43062e5ba9cac0f22
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-06 17:02:39 +0100

    Video 62.2)

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
 # Create your views here.
@@ -16,6 +16,5 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
     
-    def get_success_url(self):
-      return reverse('pages:pages')
