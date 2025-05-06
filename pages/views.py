bd0630e13ffe58cc43c130408ec976da6748e6ed
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-06 16:10:09 +0100

    Video 61) Revision App [Pages] ListView y DetailView

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Page
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
-
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageListView(ListView):
+  model = Page
+  template_name = 'pages/pages_list.html'
+   
+class PageDetailView(DetailView):
+  model = Page
+  template_name = 'pages/page_detail.html'
