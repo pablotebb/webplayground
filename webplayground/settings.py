e516f1d8df3ca3233923a3909fa87f9ddd2ff579
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-15 17:07:44 +0100

    Video 75) Restauracion de contrasena

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Auth redirects
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+# Emails 
+if DEBUG:
+  EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+  EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+else:
+  # Aquí hay que configurar un email real para producción
+  pass
