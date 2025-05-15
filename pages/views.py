aebf9008a82c45a21941484b4de9893460e10785
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-15 10:22:11 +0100

    Video 66) Creando un Mixin de identificacion

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,20 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+
+class StaffRequiredMixin(object):
+  """
+  Este mixin requerirá que el usuario sea miembro del staff
+  """
+  def dispatch(self, request, *args, **kwargs):
+    if not request.user.is_staff:
+      return redirect(reverse_lazy('admin:login'))
+    return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 
 # Create your views here.
 class PageListView(ListView):
@@ -14,12 +26,23 @@ class PageDetailView(DetailView):
   model = Page
   template_name = 'pages/page_detail.html'
   
-class PageCreate(CreateView):
+# class PageCreate(CreateView):
+#     model = Page
+#     form_class = PageForm
+#     success_url = reverse_lazy('pages:pages')
+    
+    # def dispatch(self, request, *args, **kwargs):
+       # print(request.user)
+      #  if not request.user.is_staff:
+      #    return redirect(reverse_lazy('admin:login'))
+      #  return super(PageCreate, self).dispatch(request, *args, **kwargs)
+      
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
     
-class PageUpdate(UpdateView):
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -27,7 +50,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
      
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy("pages:pages")
     
