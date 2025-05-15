e95fc54e1cd0ee62cca8b3d0a739131c8b39d4d0
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-15 10:41:36 +0100

    Video 67) Usando decoradores de identificacion

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
 from .models import Page
@@ -11,9 +13,8 @@ class StaffRequiredMixin(object):
   """
   Este mixin requerirá que el usuario sea miembro del staff
   """
+  @method_decorator(staff_member_required)
   def dispatch(self, request, *args, **kwargs):
-    if not request.user.is_staff:
-      return redirect(reverse_lazy('admin:login'))
     return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
@@ -37,12 +38,14 @@ class PageDetailView(DetailView):
       #    return redirect(reverse_lazy('admin:login'))
       #  return super(PageCreate, self).dispatch(request, *args, **kwargs)
       
-class PageCreate(StaffRequiredMixin, CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-    
-class PageUpdate(StaffRequiredMixin, UpdateView):
+  
+@method_decorator(staff_member_required, name='dispatch')  
+class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -50,7 +53,8 @@ class PageUpdate(StaffRequiredMixin, UpdateView):
     def get_success_url(self):
        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
      
-class PageDelete(StaffRequiredMixin, DeleteView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy("pages:pages")
     
