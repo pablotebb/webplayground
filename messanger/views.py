3af7d072e76dd93471e6cc6f76320afe7d4e7bf7
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-17 17:03:08 +0100

    Video 92) Urls, vistas y templates

--- messanger/views.py
+++ messanger/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from .models import Thread
+from django.http import Http404
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+# @method_decorator(login_required, name="dispatch")
+# class ThreadList(ListView):
+  # model = Thread 
+  
+  # def get_queryset(self):
+  #   queryset = super(ThreadList, self).get_queryset()
+  #   return queryset.filter(users=self.request.user)
+  
+@method_decorator(login_required, name="dispatch")
+class ThreadList(TemplateView):
+  template_name = 'messanger/thread_list.html'
+ 
+@method_decorator(login_required, name="dispatch") 
+class ThreadDetail(DetailView):
+  model = Thread 
+  
+  def get_object(self):
+    obj = super(ThreadDetail, self).get_object()
+    if self.request.user not in obj.users.all():
+      raise Http404
+    return obj 
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messanger.urls import messenger_patterns
 import django.contrib.auth.urls
 from django.conf import settings
 
@@ -30,6 +31,8 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),
     # Paths de profiles
     path('profiles/', include(profiles_patterns)),
+    # Paths de Messanger
+    path('messenger/', include(messenger_patterns)),
 ]
 
 
