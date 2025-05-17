e03255df9f31d5669fd549e2f983fea4cbd14e3f
pablotebb <4356464+pablotebb@users.noreply.github.com>
2025-05-17 10:06:01 +0100

    Video 90.2) TDD (2) Refactorizacion

--- messanger/models.py
+++ messanger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Message(models.Model):
@@ -13,3 +14,24 @@ class Message(models.Model):
 class Thread(models.Model):
   users = models.ManyToManyField(User, related_name='threads')
   messages = models.ManyToManyField(Message)
+  
+def messages_changed(sender, **kwargs):
+  instance = kwargs.pop('instance', None)
+  action = kwargs.pop('action', None)
+  pk_set = kwargs.pop('pk_set', None)
+  print(instance, action, pk_set)
+  
+  false_pk_set = set()
+  if action is 'pre_add':
+    for msg_pk in pk_set:
+      msg = Message.objects.get(pk=msg_pk)
+      if msg.user not in instance.users.all():
+        print('Ups, ({}) no forma parte del hilo'.format(msg.user))
+        # pk_set.remove(msg_pk)            En un mundo ideal... No funciona
+        false_pk_set.add(msg_pk)
+        
+  # Buscar los mensajes de false_pk_set que si están en pk_set y los borramos de pk_set
+  pk_set.difference_update(false_pk_set)
+
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
